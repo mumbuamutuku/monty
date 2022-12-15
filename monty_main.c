0dadff7116cbd7ed1a0514bf8471c105ec52bfc4
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-15 22:18:39 +0300

    errors fixed

--- monty_main.c
+++ monty_main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	char *content;
 	FILE *file;
 	size_t size = 0;
-	ssize_t read_line = 1;
+	size_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
