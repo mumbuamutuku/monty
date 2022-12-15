6debc10f06d8a92fce3726d80b3450d9704e2bdf
mumbuamutuku <mumbuamutuku@gmail.com>
2022-12-15 22:14:06 +0300

    errors fixed

--- monty.h
+++ monty.h
@@ -55,7 +55,9 @@ void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void f_queue(stack_t **head, unsigned int counter);
 void free_stack(stack_t *head);
-oid f_stack(stack_t **head, unsigned int counter);
-
+void f_stack(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
 
 #endif
