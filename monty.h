34c593f6633c0d69e9284a6e0752527cf8567fab
clintmadeit <clintonbrirdley@gmail.com>
2022-12-16 11:28:24 +0300

    main

--- monty.h
+++ monty.h
@@ -3,11 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <string.h>
-
-extern bus_t bus;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -41,30 +36,3 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * functions
-*/
-void f_push(stack_t **head, unsigned int counter);
-void f_pall(stack_t **head, unsigned int counter);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void f_queue(stack_t **head, unsigned int counter);
-void free_stack(stack_t *head);
-void f_stack(stack_t **head, unsigned int counter);
-void f_pint(stack_t **head, unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-
-
-int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
-#endif
